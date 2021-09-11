290a5b5adaa3553b4b3f00454f0a7384ed2bb1f3
mohiandipta <mohianmostofa@gmail.com>
2021-09-11 12:04:08 +0600

    database connection string added

--- CRUD Stored Procedure  with asp.net and sql/Default.aspx.cs
+++ CRUD Stored Procedure  with asp.net and sql/Default.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -13,6 +14,9 @@ namespace CRUD_Stored_Procedure__with_asp.net_and_sql
         {
 
         }
+
+        // sqlConnection
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-0NFD8E2\SQLEXPRESS;Initial Catalog=MyTest_DB;Integrated Security=True");
 
         protected void Button1_Click(object sender, EventArgs e)
         {
